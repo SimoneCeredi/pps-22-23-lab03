626cf20ad67492fac733f211ee70f9d207909693
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-03-20 12:06:29 +0100

    Completed flatMap task 1.3

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -35,6 +35,12 @@ object Lists extends App:
       case Cons(h, t) => Cons(h, append(t, right))
       case Nil() => right
 
+    def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match
+      case Nil() => Nil()
+      case Cons(h, t) => f(h) match
+        case Nil() => Nil()
+        case Cons(head, tail) => append(Cons(head, tail), flatMap(t)(f))
+
 
   val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
   println(List.sum(l)) // 60
--- src/test/scala/u03/ListTest.scala
+++ src/test/scala/u03/ListTest.scala
@@ -28,11 +28,11 @@ class ListTest:
     assertEquals(Nil(), drop(l, 5))
     assertEquals(Nil(), drop(l, -5))
 
-  /**
-   * val tail = Cons (40 , Nil () )
-   * append ( lst , tail ) // Cons (10 , Cons (20 , Cons (30 , Cons (40 , Nil ()))))
-   */
   @Test def testAppend() =
     val tail = Cons(40, Nil())
     assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Nil())))), append(l, tail))
     assertEquals(tail, append(Nil(), tail))
+  
+  @Test def testFlatMap() =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(l)(v => Cons(v + 1, Nil())))
+    assertEquals(Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))), flatMap(l)(v => Cons(v + 1, Cons(v + 2, Nil()))))
